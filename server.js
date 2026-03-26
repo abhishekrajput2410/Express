d9120a53ab14d132f50f32cfc89d70188180d8f8
abhishekrajput2410 <abhishekkumarrajput2410@gmail.com>
2026-03-26 15:18:57 +0530

    26/03/2026

--- server.js
+++ server.js
@@ -1,39 +1,57 @@
 const express = require('express');
 const app = express();
 
-// require('dotenv').config({
-//     path: '.env.development'
-// });
+// // require('dotenv').config({
+// //     path: '.env.development'
+// // });
 
-// const PORT = process.env.PORT || 5000;
+// // const PORT = process.env.PORT || 5000;
+
+// // app.use(express.json());
+
+// // const userRoutes = require('./Routes/userRoutes');
+// // app.use('/', userRoutes);
+
+// // app.listen(PORT, () => {
+// //     console.log(`Server running on port ${PORT}`);
+// // });
+
 
 // app.use(express.json());
 
-// const userRoutes = require('./Routes/userRoutes');
-// app.use('/', userRoutes);
+// const productRoutes = require("./Routes/productRoutes");
+// const userRoutes = require("./Routes/userRoutes");
+// const orderRoutes = require("./Routes/orderRoutes");
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
+
+// app.use("/api/products", productRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("E-Commerce API Running 🚀");
 // });
 
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+require('dotenv').config();
 
 app.use(express.json());
 
-// Import routes
-const productRoutes = require("./Routes/productRoutes");
-const userRoutes = require("./Routes/userRoutes");
-const orderRoutes = require("./Routes/orderRoutes");
+const authRoutes = require('./Routes/authRoutes');
 
+app.use('/api/auth', authRoutes);
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-
-app.get("/", (req, res) => {
-  res.send("E-Commerce API Running 🚀");
+app.get('/', (req, res) => {
+    res.send('API is running...');
 });
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
