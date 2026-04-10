b89906715143417e4fd6f3d9e9c05d2b41b255a2
abhishekrajput2410 <abhishekkumarrajput2410@gmail.com>
2026-04-10 10:33:30 +0530

    10/02/2026

--- Routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createOrder, getOrders } = require("../Controllers/orderController");
-
-router.post("/", createOrder);
-router.get("/", getOrders);
-
-module.exports = router;
